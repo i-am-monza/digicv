d8067df86d18b1ef0a2e44a92195f0f2637bb1ce
Monza <monwabisidingane@gmail.com>
2020-04-05 16:05:07 +0200

    Updates

---
+++ precache-manifest.47cd887498d2d965076271f367b4e1fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e7e51c023bfc7a8043cc7d093dba46",
+    "url": "/digicv/index.html"
+  },
+  {
+    "revision": "de0d632b49cafaf1a6ad",
+    "url": "/digicv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e44b1790655b580666b",
+    "url": "/digicv/static/js/2.8cbd35ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digicv/static/js/2.8cbd35ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0d632b49cafaf1a6ad",
+    "url": "/digicv/static/js/main.b4c5efde.chunk.js"
+  },
+  {
+    "revision": "f393fafb05af68c6da48",
+    "url": "/digicv/static/js/runtime-main.dea06426.js"
+  }
+]);
