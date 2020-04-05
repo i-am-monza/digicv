da9ec1faeb27e934c5da50df56f412ec14f08759
LiamCJ <liamcjones2000@gmail.com>
2020-04-05 16:50:34 +0200

    Implemented new props to tae advantage of the 6 new class switches

--- src/components/buttons/buttonVariety1.js
+++ src/components/buttons/buttonVariety1.js
@@ -5,16 +5,21 @@ import './buttonVariety1.css';
 export const Button = props => {
     
     return (
-        <button
+        <a
             id="button-var-1"
             className={ props.switch }
             style={{
                 fontSize: props.fontSize,
                 color: props.fontColour,
-                background: props.background
+                backgroundColor:  `${props.background}`,
+                background:`linear-gradient(to bottom, ${props.top_background} 5%, ${props.bottom_background} 100%)`,
+                boxShadow: `3px 4px 0px 0px ${props.boxShadow}`,
+                border:`1px solid ${props.borderColour}`,
+                textShadow:`0px 1px 0px ${props.textShadow}`,
+
             }}
         >
             {props.text}
-        </button>
+        </a>
     );
 }
